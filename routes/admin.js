2245009578943e07506e857a9e1af08183572b60
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-10-25 15:31:55 -0400

    filtering/adding namespace for admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,16 @@ const router = express.Router();
 
 
 //middleware function implemented use app.use
+//  /admin/add-product => GET 
 router.get('/add-product', (req, res, next) => {
     //console.log("in the middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 
     //next();//this allows the request to continue to the next middleware
 })
 
-router.post('/product', (req, res, next) => {
+//  /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/');
 })
