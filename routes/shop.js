4a4cdcd5c7c9079c1ed0d49d465b638487634d28
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-11-08 15:01:41 -0500

    displaying dynamic data added in products page

--- routes/shop.js
+++ routes/shop.js
@@ -10,9 +10,10 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
     //console.log("in another middleware");
-    console.log("shop.js", adminData.products);
+    //console.log("shop.js", adminData.products);
+    const products = adminData.products;
 
-    res.render('shop')
+    res.render('shop', { prods: products, docTitle: 'Shopppe' })
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
