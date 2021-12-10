1ae0aad2427b7a9870902b3a9e0dd77ae9f396d6
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-10 12:19:04 -0500

    added mysql2 lib, retrieve data from local db executed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./util/database');
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -12,6 +14,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const ErrorController = require('./controllers/error');
 
+db.execute('SELECT * FROM Products')
+    .then((result) => {
+        console.log(result[0]);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
