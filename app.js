6cba807d33f0ae3c82f67567ce09ebb517fec1e8
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-10 12:31:38 -0500

    fetching products from local db and rendering on Shop and Products page

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const db = require('./util/database');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,14 +12,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const ErrorController = require('./controllers/error');
-
-db.execute('SELECT * FROM Products')
-    .then((result) => {
-        console.log(result[0]);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
