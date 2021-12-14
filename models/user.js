bc9134b6e5bfdfebc3d169db85f07d2c30a07f54
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-14 16:43:00 -0500

    only storing id for the product for Add to cart functionality

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ module.exports = class User {
         //     return cp._id === product._id;
         // });
         console.log("in add to cart method", this);
-        const updatedCart = { items: [{ ...product, quantity: 1 }] };
+        const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
         const db = getDb();
         return db
             .collection('users')
