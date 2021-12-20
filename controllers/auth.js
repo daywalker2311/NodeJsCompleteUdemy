1bb9131636f36bfec66e60881fa16f129414e663
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-20 16:44:03 -0500

    deleting session via logout btn

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     //const isLoggedIn = req.get('Cookie').trim().split('=')[1];
     console.log(req.session.isLoggedIn);
@@ -11,7 +13,18 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
     //res.setHeader('Set-Cookie', 'loggedIn=true');
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('61bb9b2101a9e209635c1ae9')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.user = user;
+            res.redirect('/');
+        })
+        .catch(err => { console.log("postLogin error : ", err) });
 
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    })
 }
