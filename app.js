e7300e9afa3820dbe104753dcc5aea6bd1a493a1
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-20 16:43:32 -0500

    added connect-mongodb-session lib; to work with server side session
    and storing the sessions in Mongodb

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
 
+const MONGODB_URI = 'mongodb+srv://mern123:mern123@devconnector.ux9ev.mongodb.net/shop?retryWrites=true&w=majority';
 
 //added mongoose for handling data and DB, its an (O)bject (D)ocument (M)apper like ORM
 const mongoose = require('mongoose');
@@ -11,6 +13,11 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 
 const app = express();
+//initializing mongodb connection with session
+const store = new MongoDbStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -25,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //implementing session in order to validate user in backend instead of using cookies in browser
-app.use(session({ secret: 'my long secret key', resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'my long secret key', resave: false, saveUninitialized: false, store: store }));
 
 
 app.use((req, res, next) => {
@@ -45,7 +52,7 @@ app.use(shopRoutes);
 app.use(authRoutes);
 app.use(ErrorController.get404Page);
 
-mongoose.connect('mongodb+srv://mern123:mern123@devconnector.ux9ev.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
     .then(result => {
         //findOne() returns the first user in the DB
         User.findOne().then(user => {
