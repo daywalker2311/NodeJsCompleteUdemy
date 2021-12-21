028344b61b29369167cdfafc91e59cf4345f413a
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-12-21 16:32:46 -0500

    user login functionality added

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,15 +14,30 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
     //res.setHeader('Set-Cookie', 'loggedIn=true');
-    User.findById('61bb9b2101a9e209635c1ae9')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                console.log(err)
-                res.redirect('/');
-            })
+    const email = req.body.email;
+    const password = req.body.password;
 
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.redirect('/login');
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err)
+                            res.redirect('/');
+                        })
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log("bcrypt compare err : ", err);
+                })
         })
         .catch(err => { console.log("postLogin error : ", err) });
 
