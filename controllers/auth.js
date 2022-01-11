7c085e9d7d48940eeedb72a461ad071cbe57509a
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2022-01-11 01:57:40 -0500

    flash message for invalid password added as well

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,6 +40,7 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         })
                     }
+                    req.flash('error', 'Invalid email or password');
                     res.redirect('/login');
                 })
                 .catch(err => {
